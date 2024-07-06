1abef645f54f634fda015d45ed3a81e768a3c063
DaniellaKatz <denissiniu@gmail.com>
2024-07-06 21:30:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+print(str("1st program"))
+print(int(9**0.5)*5)
+print(str("2nd program"))
+print(9.99>9.98) and (1000!=1000.1)
+print(str("3nd program"))
+a = 1234%1000//10
+b = (5678%1000//10)
+integers = a+b
+print(integers)
+print(str("4nd program"))
+a1 = 13.42
+b1 = 42.13
+print(a1 != b1)
+
+
+
+
+
+
